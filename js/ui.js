d76644240527c2dd75215833e66a106a6b60fc9f
Greg Herlein <gherlein@herlein.com>
2025-03-08 05:37:54 -0800

    updating

--- js/ui.js
+++ js/ui.js
@@ -1,25 +1,20 @@
 (function (window, document) {
 
-    // we fetch the elements each time because docusaurus removes the previous
-    // element references on page navigation
-    function getElements() {
-        return {
-            layout: document.getElementById('layout'),
-            menu: document.getElementById('menu'),
-            menuLink: document.getElementById('menuLink')
-        };
-    }
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink'),
+        content  = document.getElementById('main');
 
     function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/);
-        var length = classes.length;
-        var i = 0;
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
 
-        for (; i < length; i++) {
-            if (classes[i] === className) {
-                classes.splice(i, 1);
-                break;
-            }
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
         }
         // The className is not found
         if (length === classes.length) {
@@ -29,26 +24,23 @@
         element.className = classes.join(' ');
     }
 
-    function toggleAll() {
+    function toggleAll(e) {
         var active = 'active';
-        var elements = getElements();
 
-        toggleClass(elements.layout, active);
-        toggleClass(elements.menu, active);
-        toggleClass(elements.menuLink, active);
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
     }
 
-    function handleEvent(e) {
-        var elements = getElements();
+    menuLink.onclick = function (e) {
+        toggleAll(e);
+    };
 
-        if (e.target.id === elements.menuLink.id) {
-            toggleAll();
-            e.preventDefault();
-        } else if (elements.menu.className.indexOf('active') !== -1) {
-            toggleAll();
+    content.onclick = function(e) {
+        if (menu.className.indexOf('active') !== -1) {
+            toggleAll(e);
         }
-    }
-
-    document.addEventListener('click', handleEvent);
+    };
 
 }(this, this.document));
